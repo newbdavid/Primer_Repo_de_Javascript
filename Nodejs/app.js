f8c7460853c022c527416280eac49eb41fa8aff6
David Moncayo <davidmoncayo4@gmail.com>
2016-11-23 14:46:07 -0500

    Cosas chevere instlaando de manera global y de manera local nuestro programa
    :dancer: :dango: :dart: :love_letter:

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,4 +1,13 @@
+//tecnologiaswebmdavid.imprimirSuma1y2()
+module.exports={
+    imprimirSuma1y2:imprimirSuma1y2,
+}
+
+
+function imprimirSuma1y2(){
 var numero = 1;
 var numero1 = 2;
 console.log(numero + numero1);
+}
+
 //corremos con node app.js donde app.js es el nombre del archivo
