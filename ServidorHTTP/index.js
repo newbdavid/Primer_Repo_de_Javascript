79641ce4134a86508fb8beacee78e9b1ebf5920d
David Moncayo <davidmoncayo4@gmail.com>
2016-11-30 14:34:43 -0500

    Igualandome
    app.get('/Usuario', function (req, res) {
    
    res.json(usuarios);
    
    })

--- ServidorHTTP/index.js
+++ ServidorHTTP/index.js
@@ -1,6 +1,22 @@
 var express = require('express')
 var app = express()
-
+var usuarios = [
+        {
+            id:1,
+            nombre:"Carlos",
+            cedula:1720825346,
+        },
+        {
+            id:2,
+            nombre:"Mario",
+            cedula:1720825346,
+        },
+        {
+            id:3,
+            nombre:"Balseca",
+            cedula:1720825346,
+        },
+    ];
 app.get('/TecnologiasWeb', function (req, res) {
   res.send('con javascript')
 })
@@ -11,20 +27,39 @@ app.post('/TecnologiasWeb', function (req, res) {
         "cedula":"1720825346"
     }
     
-    console.log(res) ;  
-    console.log("Response Headers");
-    console.log(res.HEADERS_RECEIVED);
-    console.log("Request Headers");
-    console.log(req.head);
+//    console.log(res);
+// console.log("Response Headers");
+// console.log(res.HEADERS_RECEIVED);
+// console.log("Request Headers");
+// console.log(req.head);
     
     usuario.apellido= 'Moncayo';
     usuario.mascotas= [];
+    
+    
+    
+    
     
     //anadimos a las cabeceras de respuesta un tokenPostman
     res.append("token",1234);
     res.json(usuario);
   
   
+})
+app.get('/Usuario', function (req, res) {
+
+    res.json(usuarios);
+    
+})
+
+app.get('/Usuario/:idUsuario',function (req,res){
+    
+    var idActual = req.params.idUsuario;
+    for (var i=0;usuarios.length;i++){
+        if(idActual == usuarios[i].id)
+            res.json(usuarios[i]);
+    }
+    res.send('I dont know such family ')
 })
 
 app.listen(5050, function () {
